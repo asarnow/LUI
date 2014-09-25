5850a626c5bde2105415a8d349cc9106466ddac3
Daniel <dasarnow@gmail.com>
2014-09-25 15:52:01 -0700

    DetectorTester Kinetic Series capability

--- DetectorTester/Form1.Designer.cs
+++ DetectorTester/Form1.Designer.cs
@@ -28,59 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.specGraph = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.imageBox = new System.Windows.Forms.PictureBox();
             this.specButton = new System.Windows.Forms.Button();
             this.imageButton = new System.Windows.Forms.Button();
             this.darkButton = new System.Windows.Forms.Button();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.blankButton = new System.Windows.Forms.Button();
+            this.commandsGroupBox = new System.Windows.Forms.GroupBox();
             this.clearButton = new System.Windows.Forms.Button();
+            this.blankButton = new System.Windows.Forms.Button();
+            this.kineticSeriesGroupBox = new System.Windows.Forms.GroupBox();
+            this.abortButton = new System.Windows.Forms.Button();
+            this.acqModePanel = new System.Windows.Forms.Panel();
+            this.imageAcqButton = new System.Windows.Forms.RadioButton();
+            this.acqModeLabel = new System.Windows.Forms.Label();
+            this.fvbButton = new System.Windows.Forms.RadioButton();
+            this.exciteCheck = new System.Windows.Forms.CheckBox();
+            this.startButton = new System.Windows.Forms.Button();
+            this.seriesLengthLabel = new System.Windows.Forms.Label();
+            this.seriesLength = new System.Windows.Forms.NumericUpDown();
             ((System.ComponentModel.ISupportInitialize)(this.specGraph)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.imageBox)).BeginInit();
-            this.groupBox1.SuspendLayout();
+            this.commandsGroupBox.SuspendLayout();
+            this.kineticSeriesGroupBox.SuspendLayout();
+            this.acqModePanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.seriesLength)).BeginInit();
             this.SuspendLayout();
             // 
             // specGraph
             // 
-            chartArea1.AxisX.IntervalAutoMode = System.Windows.Forms.DataVisualization.Charting.IntervalAutoMode.VariableCount;
-            chartArea1.AxisX.Maximum = 1023D;
-            chartArea1.AxisX.Minimum = 0D;
-            chartArea1.AxisY.IntervalAutoMode = System.Windows.Forms.DataVisualization.Charting.IntervalAutoMode.VariableCount;
-            chartArea1.AxisY.IntervalOffsetType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Number;
-            chartArea1.AxisY.IntervalType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Number;
-            chartArea1.AxisY.Maximum = 65536D;
-            chartArea1.AxisY.Minimum = 0D;
-            chartArea1.Name = "specArea";
-            this.specGraph.ChartAreas.Add(chartArea1);
-            this.specGraph.Location = new System.Drawing.Point(9, 10);
-            this.specGraph.Margin = new System.Windows.Forms.Padding(2);
+            chartArea2.AxisX.IntervalAutoMode = System.Windows.Forms.DataVisualization.Charting.IntervalAutoMode.VariableCount;
+            chartArea2.AxisX.Maximum = 1023D;
+            chartArea2.AxisX.Minimum = 0D;
+            chartArea2.AxisY.IntervalAutoMode = System.Windows.Forms.DataVisualization.Charting.IntervalAutoMode.VariableCount;
+            chartArea2.AxisY.IntervalOffsetType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Number;
+            chartArea2.AxisY.IntervalType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Number;
+            chartArea2.AxisY.Maximum = 65536D;
+            chartArea2.AxisY.Minimum = 0D;
+            chartArea2.Name = "specArea";
+            this.specGraph.ChartAreas.Add(chartArea2);
+            this.specGraph.Location = new System.Drawing.Point(12, 12);
+            this.specGraph.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.specGraph.Name = "specGraph";
-            series1.ChartArea = "specArea";
-            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
-            series1.Name = "spec";
-            this.specGraph.Series.Add(series1);
-            this.specGraph.Size = new System.Drawing.Size(614, 297);
+            series2.ChartArea = "specArea";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Name = "spec";
+            this.specGraph.Series.Add(series2);
+            this.specGraph.Size = new System.Drawing.Size(819, 366);
             this.specGraph.TabIndex = 0;
             this.specGraph.Text = "chart1";
             // 
             // imageBox
             // 
-            this.imageBox.Location = new System.Drawing.Point(9, 312);
-            this.imageBox.Margin = new System.Windows.Forms.Padding(2);
+            this.imageBox.Location = new System.Drawing.Point(12, 384);
+            this.imageBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.imageBox.Name = "imageBox";
-            this.imageBox.Size = new System.Drawing.Size(768, 208);
+            this.imageBox.Size = new System.Drawing.Size(1024, 256);
             this.imageBox.TabIndex = 1;
             this.imageBox.TabStop = false;
             // 
             // specButton
             // 
-            this.specButton.Location = new System.Drawing.Point(10, 54);
-            this.specButton.Margin = new System.Windows.Forms.Padding(2);
+            this.specButton.Location = new System.Drawing.Point(13, 66);
+            this.specButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.specButton.Name = "specButton";
-            this.specButton.Size = new System.Drawing.Size(62, 24);
+            this.specButton.Size = new System.Drawing.Size(83, 30);
             this.specButton.TabIndex = 2;
             this.specButton.Text = "Capture";
             this.specButton.UseVisualStyleBackColor = true;
@@ -88,10 +101,10 @@
             // 
             // imageButton
             // 
-            this.imageButton.Location = new System.Drawing.Point(10, 25);
-            this.imageButton.Margin = new System.Windows.Forms.Padding(2);
+            this.imageButton.Location = new System.Drawing.Point(13, 31);
+            this.imageButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.imageButton.Name = "imageButton";
-            this.imageButton.Size = new System.Drawing.Size(62, 24);
+            this.imageButton.Size = new System.Drawing.Size(83, 30);
             this.imageButton.TabIndex = 3;
             this.imageButton.Text = "Image";
             this.imageButton.UseVisualStyleBackColor = true;
@@ -99,66 +112,186 @@
             // 
             // darkButton
             // 
-            this.darkButton.Location = new System.Drawing.Point(77, 25);
-            this.darkButton.Margin = new System.Windows.Forms.Padding(2);
+            this.darkButton.Location = new System.Drawing.Point(103, 31);
+            this.darkButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.darkButton.Name = "darkButton";
-            this.darkButton.Size = new System.Drawing.Size(62, 24);
+            this.darkButton.Size = new System.Drawing.Size(83, 30);
             this.darkButton.TabIndex = 4;
             this.darkButton.Text = "Dark";
             this.darkButton.UseVisualStyleBackColor = true;
             this.darkButton.Click += new System.EventHandler(this.darkButton_Click);
             // 
-            // groupBox1
+            // commandsGroupBox
             // 
-            this.groupBox1.Controls.Add(this.clearButton);
-            this.groupBox1.Controls.Add(this.blankButton);
-            this.groupBox1.Controls.Add(this.imageButton);
-            this.groupBox1.Controls.Add(this.specButton);
-            this.groupBox1.Controls.Add(this.darkButton);
-            this.groupBox1.Location = new System.Drawing.Point(627, 10);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
-            this.groupBox1.Size = new System.Drawing.Size(150, 297);
-            this.groupBox1.TabIndex = 5;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Commands";
-            // 
-            // blankButton
-            // 
-            this.blankButton.Location = new System.Drawing.Point(77, 54);
-            this.blankButton.Margin = new System.Windows.Forms.Padding(2);
-            this.blankButton.Name = "blankButton";
-            this.blankButton.Size = new System.Drawing.Size(62, 24);
-            this.blankButton.TabIndex = 5;
-            this.blankButton.Text = "Blank";
-            this.blankButton.UseVisualStyleBackColor = true;
-            this.blankButton.Click += new System.EventHandler(this.blankButton_Click);
+            this.commandsGroupBox.Controls.Add(this.clearButton);
+            this.commandsGroupBox.Controls.Add(this.blankButton);
+            this.commandsGroupBox.Controls.Add(this.imageButton);
+            this.commandsGroupBox.Controls.Add(this.specButton);
+            this.commandsGroupBox.Controls.Add(this.darkButton);
+            this.commandsGroupBox.Location = new System.Drawing.Point(836, 12);
+            this.commandsGroupBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.commandsGroupBox.Name = "commandsGroupBox";
+            this.commandsGroupBox.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.commandsGroupBox.Size = new System.Drawing.Size(200, 142);
+            this.commandsGroupBox.TabIndex = 5;
+            this.commandsGroupBox.TabStop = false;
+            this.commandsGroupBox.Text = "Commands";
             // 
             // clearButton
             // 
-            this.clearButton.Location = new System.Drawing.Point(10, 83);
+            this.clearButton.Location = new System.Drawing.Point(13, 102);
+            this.clearButton.Margin = new System.Windows.Forms.Padding(4);
             this.clearButton.Name = "clearButton";
-            this.clearButton.Size = new System.Drawing.Size(62, 24);
+            this.clearButton.Size = new System.Drawing.Size(83, 30);
             this.clearButton.TabIndex = 6;
             this.clearButton.Text = "Clear";
             this.clearButton.UseVisualStyleBackColor = true;
             this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
             // 
+            // blankButton
+            // 
+            this.blankButton.Location = new System.Drawing.Point(103, 66);
+            this.blankButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.blankButton.Name = "blankButton";
+            this.blankButton.Size = new System.Drawing.Size(83, 30);
+            this.blankButton.TabIndex = 5;
+            this.blankButton.Text = "Blank";
+            this.blankButton.UseVisualStyleBackColor = true;
+            this.blankButton.Click += new System.EventHandler(this.blankButton_Click);
+            // 
+            // kineticSeriesGroupBox
+            // 
+            this.kineticSeriesGroupBox.Controls.Add(this.abortButton);
+            this.kineticSeriesGroupBox.Controls.Add(this.acqModePanel);
+            this.kineticSeriesGroupBox.Controls.Add(this.exciteCheck);
+            this.kineticSeriesGroupBox.Controls.Add(this.startButton);
+            this.kineticSeriesGroupBox.Controls.Add(this.seriesLengthLabel);
+            this.kineticSeriesGroupBox.Controls.Add(this.seriesLength);
+            this.kineticSeriesGroupBox.Location = new System.Drawing.Point(838, 160);
+            this.kineticSeriesGroupBox.Name = "kineticSeriesGroupBox";
+            this.kineticSeriesGroupBox.Size = new System.Drawing.Size(200, 218);
+            this.kineticSeriesGroupBox.TabIndex = 6;
+            this.kineticSeriesGroupBox.TabStop = false;
+            this.kineticSeriesGroupBox.Text = "Kinetic Series";
+            // 
+            // abortButton
+            // 
+            this.abortButton.Location = new System.Drawing.Point(11, 182);
+            this.abortButton.Name = "abortButton";
+            this.abortButton.Size = new System.Drawing.Size(83, 30);
+            this.abortButton.TabIndex = 7;
+            this.abortButton.Text = "Abort";
+            this.abortButton.UseVisualStyleBackColor = true;
+            this.abortButton.Click += new System.EventHandler(this.abortButton_Click);
+            // 
+            // acqModePanel
+            // 
+            this.acqModePanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.acqModePanel.Controls.Add(this.imageAcqButton);
+            this.acqModePanel.Controls.Add(this.acqModeLabel);
+            this.acqModePanel.Controls.Add(this.fvbButton);
+            this.acqModePanel.Location = new System.Drawing.Point(6, 21);
+            this.acqModePanel.Name = "acqModePanel";
+            this.acqModePanel.Size = new System.Drawing.Size(103, 76);
+            this.acqModePanel.TabIndex = 9;
+            // 
+            // imageAcqButton
+            // 
+            this.imageAcqButton.AutoSize = true;
+            this.imageAcqButton.Location = new System.Drawing.Point(3, 47);
+            this.imageAcqButton.Name = "imageAcqButton";
+            this.imageAcqButton.Size = new System.Drawing.Size(67, 21);
+            this.imageAcqButton.TabIndex = 8;
+            this.imageAcqButton.TabStop = true;
+            this.imageAcqButton.Text = "Image";
+            this.imageAcqButton.UseVisualStyleBackColor = true;
+            // 
+            // acqModeLabel
+            // 
+            this.acqModeLabel.AutoSize = true;
+            this.acqModeLabel.Location = new System.Drawing.Point(0, 0);
+            this.acqModeLabel.Name = "acqModeLabel";
+            this.acqModeLabel.Size = new System.Drawing.Size(71, 17);
+            this.acqModeLabel.TabIndex = 0;
+            this.acqModeLabel.Text = "Acq Mode";
+            // 
+            // fvbButton
+            // 
+            this.fvbButton.AutoSize = true;
+            this.fvbButton.Location = new System.Drawing.Point(3, 20);
+            this.fvbButton.Name = "fvbButton";
+            this.fvbButton.Size = new System.Drawing.Size(55, 21);
+            this.fvbButton.TabIndex = 7;
+            this.fvbButton.TabStop = true;
+            this.fvbButton.Text = "FVB";
+            this.fvbButton.UseVisualStyleBackColor = true;
+            // 
+            // exciteCheck
+            // 
+            this.exciteCheck.AutoSize = true;
+            this.exciteCheck.Location = new System.Drawing.Point(115, 21);
+            this.exciteCheck.Name = "exciteCheck";
+            this.exciteCheck.Size = new System.Drawing.Size(79, 38);
+            this.exciteCheck.TabIndex = 7;
+            this.exciteCheck.Text = " Excite\r\n sample";
+            this.exciteCheck.UseVisualStyleBackColor = true;
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(101, 182);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(83, 30);
+            this.startButton.TabIndex = 2;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // seriesLengthLabel
+            // 
+            this.seriesLengthLabel.AutoSize = true;
+            this.seriesLengthLabel.Location = new System.Drawing.Point(6, 102);
+            this.seriesLengthLabel.Name = "seriesLengthLabel";
+            this.seriesLengthLabel.Size = new System.Drawing.Size(96, 17);
+            this.seriesLengthLabel.TabIndex = 1;
+            this.seriesLengthLabel.Text = "Series Length";
+            // 
+            // seriesLength
+            // 
+            this.seriesLength.Location = new System.Drawing.Point(108, 100);
+            this.seriesLength.Maximum = new decimal(new int[] {
+            3600,
+            0,
+            0,
+            0});
+            this.seriesLength.Name = "seriesLength";
+            this.seriesLength.Size = new System.Drawing.Size(54, 22);
+            this.seriesLength.TabIndex = 0;
+            this.seriesLength.Value = new decimal(new int[] {
+            180,
+            0,
+            0,
+            0});
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(782, 527);
-            this.Controls.Add(this.groupBox1);
+            this.ClientSize = new System.Drawing.Size(1043, 649);
+            this.Controls.Add(this.kineticSeriesGroupBox);
+            this.Controls.Add(this.commandsGroupBox);
             this.Controls.Add(this.imageBox);
             this.Controls.Add(this.specGraph);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "DetectorTester";
             ((System.ComponentModel.ISupportInitialize)(this.specGraph)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.imageBox)).EndInit();
-            this.groupBox1.ResumeLayout(false);
+            this.commandsGroupBox.ResumeLayout(false);
+            this.kineticSeriesGroupBox.ResumeLayout(false);
+            this.kineticSeriesGroupBox.PerformLayout();
+            this.acqModePanel.ResumeLayout(false);
+            this.acqModePanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.seriesLength)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -170,9 +303,19 @@
         private System.Windows.Forms.Button specButton;
         private System.Windows.Forms.Button imageButton;
         private System.Windows.Forms.Button darkButton;
-        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox commandsGroupBox;
         private System.Windows.Forms.Button blankButton;
         private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.GroupBox kineticSeriesGroupBox;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label seriesLengthLabel;
+        private System.Windows.Forms.NumericUpDown seriesLength;
+        private System.Windows.Forms.RadioButton imageAcqButton;
+        private System.Windows.Forms.RadioButton fvbButton;
+        private System.Windows.Forms.CheckBox exciteCheck;
+        private System.Windows.Forms.Panel acqModePanel;
+        private System.Windows.Forms.Label acqModeLabel;
+        private System.Windows.Forms.Button abortButton;
     }
 }
 
--- DetectorTester/Form1.cs
+++ DetectorTester/Form1.cs
@@ -19,17 +19,32 @@ namespace DetectorTester
         private int[] counts;
         private int[] image;
 
+        private BackgroundWorker worker;
+
+        struct WorkParameters
+        {
+            public int NSteps { get; set; }
+            public int AcqMode { get; set; }
+            public int ReadMode { get; set; }
+            public int TriggerMode{ get; set; }
+            public bool Excite { get; set; }
+        }
+
         public Form1(Commander commander)
         {
             Commander = commander;
+
+            worker = new BackgroundWorker();
+            worker.WorkerSupportsCancellation = true;
+            worker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.KineticSeriesAsync);
+            worker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.ReportProgress);
+
             InitializeComponent();
         }
 
-        private void imageButton_Click(object sender, EventArgs e)
+        private void ShowImage(int[] image)
         {
-            image = Commander.Camera.GetFullResolutionImage();
             Util.normalizeArray(image, 255);
-
             Bitmap picture = new Bitmap(1024, 256);
             for (int x = 0; x < picture.Width; x++)
             {
@@ -39,8 +54,12 @@ namespace DetectorTester
                     picture.SetPixel(x, y, c);
                 }
             }
-
             imageBox.Image = picture;
+        }
+
+        private void imageButton_Click(object sender, EventArgs e)
+        {
+            ShowImage( Commander.Camera.GetFullResolutionImage() );
         }
 
         private void darkButton_Click(object sender, EventArgs e)
@@ -57,14 +76,37 @@ namespace DetectorTester
             Commander.Camera.SetTriggerMode(Constants.TrigModeExternalExposure);
             Commander.Camera.SetReadMode(Constants.ReadModeFVB);
             blank = Commander.Flash();
+            ApplyDark(blank);
+        }
 
-            if (dark != null)
+        private void ApplyBlank(int[] data)
+        {
+            if (blank != null)
             {
-                for (int i = 0; i < counts.Length; i++)
+                for (int i = 0; i < data.Length; i++)
                 {
-                    blank[i] -= dark[i];
+                    data[i] = blank[i] - data[i];
                 }
             }
+        }
+
+        private void ApplyDark(int[] data)
+        {
+            if (dark != null)
+            {
+                for (int i = 0; i < data.Length; i++)
+                {
+                    data[i] -= dark[i];
+                }
+            }
+        }
+
+        private void AddSpec(int[] data)
+        {
+            for (int i = 0; i < data.Length; i++)
+                specGraph.Series["spec"].Points.AddXY(i, data[i]);
+
+            specGraph.Series["spec"].ChartArea = "specArea";
         }
 
         private void specButton_Click(object sender, EventArgs e)
@@ -73,30 +115,9 @@ namespace DetectorTester
             Commander.Camera.SetTriggerMode(Constants.TrigModeExternalExposure);
             Commander.Camera.SetReadMode(Constants.ReadModeFVB);
             counts = Commander.Flash();
-
-            if (dark != null)
-            {
-                for (int i = 0; i < counts.Length; i++)
-                {
-                    counts[i] -= dark[i];
-                }
-            }
-
-            if (blank != null)
-            {
-                for (int i = 0; i < counts.Length; i++)
-                {
-                    counts[i] = blank[i] - counts[i];
-                }
-            }
-
-            for (int i = 0; i < counts.Length; i++)
-                specGraph.Series["spec"].Points.AddXY(i, counts[i]);
-
-            specGraph.Series["spec"].ChartArea = "specArea";
-
-            dark = null;
-            blank = null;
+            ApplyDark(counts);
+            ApplyBlank(counts);
+            AddSpec(counts);
         }
 
         protected override void OnFormClosing(FormClosingEventArgs e)
@@ -108,6 +129,125 @@ namespace DetectorTester
         private void clearButton_Click(object sender, EventArgs e)
         {
             specGraph.Series["spec"].Points.Clear();
+        }
+
+        private void abortButton_Click(object sender, EventArgs e)
+        {
+            worker.CancelAsync();
+        }
+
+        private bool BlankDialog(int readMode)
+        {
+            DialogResult result = MessageBox.Show("Please insert blank",
+                "Blank",
+                MessageBoxButtons.OKCancel);
+
+            if (result == DialogResult.OK)
+            {
+                if (readMode == Constants.ReadModeFVB) blank = Commander.Flash();
+                else if (readMode == Constants.ReadModeImage) blank = Commander.Camera.GetFullResolutionImage();
+            }
+            else
+            {
+                return false;
+            }
+
+            result = MessageBox.Show("Continue when ready",
+                "Continue",
+                MessageBoxButtons.OKCancel);
+
+            if (result == DialogResult.Cancel) return false;
+
+            return true;
+        }
+
+        private void ReportProgress(object sender, ProgressChangedEventArgs e)
+        {
+            int readMode = (int)sender;
+            if (readMode == Constants.ReadModeFVB) AddSpec(counts);
+            else if (readMode == Constants.ReadModeImage) ShowImage(image);
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            blank = null;
+            dark = null;
+            counts = null;
+            image = null;
+
+            WorkParameters parameters = new WorkParameters();
+            parameters.AcqMode = Constants.AcqModeSingle;
+            parameters.TriggerMode = Constants.TrigModeExternalExposure;
+
+            Commander.Camera.SetAcquisitionMode(parameters.AcqMode);
+            Commander.Camera.SetTriggerMode(parameters.TriggerMode);
+
+            parameters.Excite = exciteCheck.Checked;
+
+            if (fvbButton.Checked)
+            {
+                parameters.ReadMode = Constants.ReadModeFVB;
+            }
+            else if (imageAcqButton.Checked)
+            {
+                parameters.ReadMode = Constants.ReadModeImage;
+            }
+
+            Commander.Camera.SetReadMode(parameters.ReadMode);
+
+            if (!BlankDialog(parameters.ReadMode)) return;
+
+            worker.RunWorkerAsync(parameters);
+
+            blank = null;
+            dark = null;
+            counts = null;
+            image = null;
+        }
+
+        private void KineticSeriesAsync(object sender, DoWorkEventArgs e)
+        {
+
+            WorkParameters parameters = (WorkParameters)e.Argument;
+
+            dark = Commander.Dark();
+
+            int nsteps = parameters.NSteps;
+            int cnt = 0;
+
+            if (parameters.Excite)
+            {
+                int[] counts = Commander.Trans();
+                ApplyDark(counts);
+                ApplyBlank(counts);
+                worker.ReportProgress(parameters.ReadMode);
+            }
+
+            while (cnt <= nsteps)
+            {
+                if (worker.CancellationPending)
+                {
+                    e.Cancel = true;
+                    return;
+                }
+
+                if (parameters.ReadMode == Constants.ReadModeFVB)
+                {
+                    counts = Commander.Flash();
+                    ApplyDark(counts);
+                    ApplyBlank(counts);
+                }
+                else if (parameters.ReadMode == Constants.ReadModeImage)
+                {
+                    image = Commander.Camera.GetFullResolutionImage();
+                    ApplyDark(image);
+                }
+
+                worker.ReportProgress(parameters.ReadMode);
+
+                cnt++;
+            }
+
         }
 
     }
