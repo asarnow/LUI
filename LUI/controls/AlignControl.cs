3f0f64acf351e1b248f8e8cdf242eccd2f693bb8
Daniel Asarnow <dasarnow@sfsu.edu>
2015-01-23 00:46:21 -0800

    Started alignment control

---
+++ LUI/controls/AlignControl.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Windows.Threading;
+using System.Runtime.CompilerServices;
+using lasercom;
+
+namespace LUI.controls
+{
+    public partial class AlignControl : UserControl
+    {
+        public Commander Commander { get; set; }
+        private BackgroundWorker worker;
+        private BackgroundWorker ioWorker;
+        private Dispatcher Dispatcher;
+        bool wait;
+
+        int selectedChannel = -1;
+
+        public enum Dialog
+        {
+            BLANK, SAMPLE, PROGRESS, PROGRESS_BLANK, PROGRESS_DARK, PROGRESS_DATA,
+            PROGRESS_CALC
+        }
+
+        public AlignControl(Commander commander)
+        {
+            InitializeComponent();
+            Commander = commander;
+            Graph.MouseClick += new MouseEventHandler(Graph_Click);
+        }
+
+        #region dialogs
+
+        private void BlockingBlankDialog()
+        {
+            DialogResult result = MessageBox.Show("Please insert blank",
+                "Blank",
+                MessageBoxButtons.OKCancel);
+            if (result == DialogResult.Cancel)
+            {
+                worker.CancelAsync();
+            }
+            wait = false;
+        }
+
+        private void BlockingSampleDialog()
+        {
+            DialogResult result = MessageBox.Show("Please insert sample",
+                    "Continue",
+                    MessageBoxButtons.OKCancel);
+            if (result == DialogResult.Cancel)
+            {
+                worker.CancelAsync();
+            }
+            wait = false;
+        }
+
+        #endregion
+
+        public void AbsorbanceSpectrumWork(object sender, DoWorkEventArgs e)
+        {
+            Commander.Camera.AcquisitionMode = AndorCamera.AcquisitionModeSingle;
+            Commander.Camera.TriggerMode = AndorCamera.TriggerModeExternalExposure;
+            Commander.Camera.ReadMode = AndorCamera.ReadModeFVB;
+            int N = (int)e.Argument;
+
+            worker.ReportProgress(0, Dialog.BLANK.ToString());
+
+            int[] BlankBuffer = Commander.Flash();
+            for (int i = 0; i < N - 1; i++)
+            {
+                if (worker.CancellationPending)
+                {
+                    e.Cancel = true;
+                    return;
+                }
+                Data.Accumulate(BlankBuffer, Commander.Flash());
+                worker.ReportProgress((i / N) * 33, Dialog.PROGRESS_BLANK.ToString());
+            }
+
+            worker.ReportProgress(33, Dialog.SAMPLE.ToString());
+
+            wait = true;
+            bool[] waitparam = { wait };
+            Dispatcher.BeginInvoke(new Action(BlockingBlankDialog), waitparam);
+            while (wait) ;
+
+            worker.ReportProgress(33, Dialog.PROGRESS_DARK.ToString());
+
+            if (worker.CancellationPending)
+            {
+                e.Cancel = true;
+                return;
+            }
+
+            int[] DarkBuffer = Commander.Dark();
+            for (int i = 0; i < N - 1; i++)
+            {
+                if (worker.CancellationPending)
+                {
+                    e.Cancel = true;
+                    return;
+                }
+                Data.Accumulate(DarkBuffer, Commander.Dark());
+                worker.ReportProgress(33 + (i / N) * 33, Dialog.PROGRESS_DARK.ToString());
+            }
+
+            worker.ReportProgress(66, Dialog.PROGRESS.ToString());
+            wait = true;
+            Dispatcher.BeginInvoke(new Action(BlockingSampleDialog), new bool[] { wait });
+            while (wait) ;
+
+            if (worker.CancellationPending)
+            {
+                e.Cancel = true;
+                return;
+            }
+
+            worker.ReportProgress(66, Dialog.PROGRESS_DATA.ToString());
+
+            int[] DataBuffer = Commander.Flash();
+            for (int i = 0; i < N - 1; i++)
+            {
+                if (worker.CancellationPending)
+                {
+                    e.Cancel = true;
+                    return;
+                }
+                Data.Accumulate(DataBuffer, Commander.Flash());
+                worker.ReportProgress(66 + (i / N) * 33, Dialog.PROGRESS_DATA.ToString());
+            }
+            worker.ReportProgress(99, Dialog.PROGRESS_CALC.ToString());
+            e.Result = Data.OpticalDensity(DataBuffer, BlankBuffer, DarkBuffer);
+        }
+
+        private void Collect_Click(object sender, EventArgs e)
+        {
+            Collect.Enabled = false;
+            int N = (int)Averages.Value;
+            worker = new BackgroundWorker();
+            worker.DoWork += new System.ComponentModel.DoWorkEventHandler(AbsorbanceSpectrumWork);
+            worker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(AbsorbanceSpectrumProgress);
+            worker.WorkerSupportsCancellation = true;
+            worker.WorkerReportsProgress = true;
+            worker.RunWorkerAsync(N);
+        }
+
+        public void AbsorbanceSpectrumProgress(object sender, ProgressChangedEventArgs e)
+        {
+            Dialog operation = (Dialog)Enum.Parse(typeof(Dialog), (string)e.UserState);
+            switch (operation)
+            {
+                case Dialog.BLANK:
+                    StatusProgress.Value = e.ProgressPercentage;
+                    ProgressLabel.Text = "Waiting";
+                    break;
+                case Dialog.SAMPLE:
+                    StatusProgress.Value = e.ProgressPercentage;
+                    ProgressLabel.Text = "Waiting";
+                    break;
+                case Dialog.PROGRESS:
+                    StatusProgress.Value = e.ProgressPercentage;
+                    ProgressLabel.Text = "Busy";
+                    break;
+                case Dialog.PROGRESS_BLANK:
+                    StatusProgress.Value = e.ProgressPercentage;
+                    ProgressLabel.Text = "Collecting blank";
+                    break;
+                case Dialog.PROGRESS_DARK:
+                    StatusProgress.Value = e.ProgressPercentage;
+                    ProgressLabel.Text = "Collecting dark";
+                    break;
+                case Dialog.PROGRESS_DATA:
+                    StatusProgress.Value = e.ProgressPercentage;
+                    ProgressLabel.Text = "Collecting data";
+                    break;
+                case Dialog.PROGRESS_CALC:
+                    StatusProgress.Value = e.ProgressPercentage;
+                    ProgressLabel.Text = "Calculating";
+                    break;
+            }
+        }
+
+        public void AbsorbanceSpectrumComplete(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (!e.Cancelled)
+            {
+                double[] OD = (double[])e.Result;
+                Graph.DrawPoints(OD);
+                Graph.ClearData();
+                ProgressLabel.Text = "Complete";
+            }
+            else
+            {
+                ProgressLabel.Text = "Aborted";
+            }
+            StatusProgress.Value = 100;
+            Collect.Enabled = true;
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            Graph.Clear();
+            Graph.Invalidate();
+        }
+
+        private void Graph_Click(object sender, MouseEventArgs e)
+        {
+            PointF p = Graph.ScreenToData(new Point(e.X, e.Y));
+            selectedChannel = (int)Math.Round(p.X);
+            RedrawLines();
+        }
+
+        private void RedrawLines()
+        {
+            Graph.ClearAnnotation();
+            Graph.Annotate(GraphControl.Annotation.VERTLINE, Graph.ColorOrder[0], selectedChannel);
+            Graph.Invalidate();
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            switch (keyData)
+            {
+                case Keys.Left:
+                    if (selectedChannel > -1)
+                    {
+                        selectedChannel = (int)Math.Max(Graph.XMin, selectedChannel - 1);
+                    }
+                    RedrawLines();
+                    break;
+                case Keys.Right:
+                    if (selectedChannel > -1)
+                    {
+                        selectedChannel = (int)Math.Min(Graph.XMax, selectedChannel + 1);
+                    }
+                    RedrawLines();
+                    break;
+                case Keys.Enter:
+                    //TODO add calibration point
+                    break;
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        void TBKeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(char.IsDigit(e.KeyChar)))
+            {
+                Keys key = (Keys)e.KeyChar;
+
+                if (!(key == Keys.Back || key == Keys.Delete))
+                {
+                    e.Handled = true;
+                }
+            }
+        }
+
+    }
+}
