60f85b77e020b8d902322d522ffbd891de4f1538
Daniel Asarnow <dasarnow@sfsu.edu>
2015-06-26 12:54:46 -0700

    Slight refactor, also raise temp before shutdown.

--- lasercom/camera/CameraTempControlled.cs
+++ lasercom/camera/CameraTempControlled.cs
@@ -1,10 +1,9 @@
 ﻿using System;
 
-
 #if x64
 using ATMCD64CS;
 #else
-
+using ATMCD32CS;
 #endif
 
 //  <summary>
@@ -16,20 +15,65 @@ namespace lasercom.camera
 
     public class CameraTempControlled:AndorCamera
     {
+        public const float TemperatureEps = 3F;
+        public const int ShutdownTemp = 5;
+
+        private int _MinTemp;
+        public int MinTemp
+        {
+            get
+            {
+                return _MinTemp;
+            }
+        }
+
+        private int _MaxTemp;
+        public int MaxTemp
+        {
+            get
+            {
+                return MaxTemp;
+            }
+        }
 
         public CameraTempControlled(string CalFile, string Dir, int InitialGain, int Temperature)
             : base(CalFile, Dir, InitialGain)
         {
+            AndorSdk.GetTemperatureRange(ref _MinTemp, ref _MaxTemp);
+            AndorSdk.CoolerON();
             EquilibrateTemperature(Temperature);
         }
 
         public void EquilibrateTemperature(int targetTemperature)
         {
-            AndorSdk.CoolerON();
+            if (targetTemperature < MinTemp || targetTemperature > MaxTemp)
+            {
+                throw new ArgumentException("Temperature out of range.");
+            }
             AndorSdk.SetTemperature(targetTemperature);
             float currentTemperature = 0;
             AndorSdk.GetTemperatureF(ref currentTemperature);
-            while ( Math.Abs(currentTemperature - targetTemperature) > Constants.TemperatureEps )
+            while ( Math.Abs(currentTemperature - targetTemperature) > TemperatureEps )
+            {
+                AndorSdk.GetTemperatureF(ref currentTemperature);
+            }
+        }
+
+        public void EquilibrateTemperature()
+        {
+            int currentTemperature = 0;
+            uint status = AndorSDK.DRV_TEMP_NOT_STABILIZED;
+            while (status != AndorSDK.DRV_TEMP_STABILIZED)
+            {
+                status = AndorSdk.GetTemperature(ref currentTemperature);
+            }
+        }
+
+        public void WaitForTemperatureIncrease(int thresholdTemperature)
+        {
+            float currentTemperature = 0;
+            AndorSdk.GetTemperatureF(ref currentTemperature);
+            while (currentTemperature < (thresholdTemperature - TemperatureEps))
             {
                 AndorSdk.GetTemperatureF(ref currentTemperature);
             }
@@ -40,6 +84,7 @@ namespace lasercom.camera
             if (AndorSdk != null)
             {
                 AndorSdk.CoolerOFF();
+                WaitForTemperatureIncrease(ShutdownTemp);
                 AndorSdk.ShutDown();
             }
         }
